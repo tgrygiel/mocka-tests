1fe93e7e266acfc0232b87a2666978cbf4e23920
Tomasz G <tomasz.g1809@gmail.com>
2019-08-20 13:35:29 +0200

    initial commit

---
+++ test/calc.spec.js
@@ -0,0 +1,10 @@
+const { Calculator } = require('../src/calc')
+
+const assert = require('assert')
+
+
+describe('Calculator', () => {
+    it('should add numbers', () => {
+        assert.equal(Calculator.add(2,2), 4)
+    })
+})
